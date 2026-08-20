8dea0abbac157921b6e38dc2df0d4871776a273f
marl <10401489+marl-exe@users.noreply.github.com>
2026-08-21 04:07:03 +0800

    Serve custom Peenoise landing page

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const path = require("path");
 const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 
 const port = Number.parseInt(process.env.PORT || "7000", 10);
 
-serveHTTP(addonInterface, { port })
+serveHTTP(addonInterface, {
+  port,
+  static: path.join(__dirname, "public"),
+})
   .then(({ url }) => {
     console.log("Addon active on:", url);
     console.log("Manifest:", `${url}/manifest.json`);
